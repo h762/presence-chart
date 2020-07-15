13f22dc4f8e5ff8a7d7d3f927fd3a02a3478c930
h762 <11020i11020@gmail.com>
2020-07-15 12:58:43 +0900

    1

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module PresenceChart
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.helper false
       g.test_framework false
